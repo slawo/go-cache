d661e724a823ba42f6bacdaea968dcfefbd585c4
Slawomir Caluch <scaluch@gmail.com>
2025-06-24 17:24:42 +0100

    fix: add missing DataWriteLock

--- datastore/synchroniser.go
+++ datastore/synchroniser.go
@@ -21,3 +21,13 @@ var (
 type DataSynchroniser interface {
 	GetWriteLock(ctx context.Context, lockID string) (DataWriteLock, error)
 }
+
+//go:generate mockery --name DataWriteLock --output mocks
+type DataWriteLock interface {
+	// Unlock releases the lock. A new lock will need to be created.
+	Unlock() error
+	// Unlocked checks if the lock is currently held.
+	Unlocked() bool
+	// WaitUnlocked returns a channel that will be closed when the lock is released.
+	WaitUnlocked() <-chan struct{}
+}
